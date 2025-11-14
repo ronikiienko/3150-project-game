88a3aa21071ea632439917dda10b0b86a15ef364
Rostyslav Onikiienko <ronikiienko@gmail.com>
2025-11-14 14:07:31 -0800

    latest

--- world.gd
+++ world.gd
@@ -9,14 +9,15 @@ extends Node2D
 const CIRCLE_TEXTURE = preload("res://img.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in 8000:
-		var circle = circle_scene.instantiate() as Circle;
-		circle.gravity_scale = 0.0;
-		circle.texture = CIRCLE_TEXTURE;
-		circle.apply_force(Vector2(randf_range(-1000.0, 1000.0), randf_range(-1000, 1000.0)));
-		circle.position = Vector2(randf_range(-1000.0, 100.0), randf_range(-1000.0, 100.0));
-		circle.radius = randf_range(1.0, 5.0);
-		add_child(circle);
+	pass
+	#for i in 200:
+		#var circle = circle_scene.instantiate() as Circle;
+		#circle.texture = CIRCLE_TEXTURE;
+		#circle.apply_force(Vector2(randf_range(-1000.0, 1000.0), randf_range(-1000, 1000.0)));
+		#circle.position = Vector2(randf_range(-1000.0, 1000.0), randf_range(-1000.0, 1000.0));
+		#circle.radius = randf_range(1.0, 5.0);
+		#add_child(circle);
+		#circle.add_to_group("bodies")
 		
 func _input(event):
 	# zoom
@@ -28,6 +29,31 @@ func _input(event):
 		
 	print(event)
 
-func _process(delta: float):
+func _physics_process(delta: float):
 	fps_label.text = "Fps: %d" % Engine.get_frames_per_second()
+	
+	var bodies = get_tree().get_nodes_in_group("bodies")
+
+	var G = 5000.0  # gravity strength constant, tune this
+	for i in bodies.size():
+		var b1 = bodies[i]
+		var p1 = b1.position
+
+		for j in range(i + 1, bodies.size()):
+			var b2 = bodies[j]
+			var p2 = b2.position
+
+			var dir = p2 - p1
+			var dist_sq = dir.length_squared()
+
+			if dist_sq < 1.0:
+				continue  # prevent blowups
+
+			var force_mag = G / dist_sq
+			var force = dir.normalized() * force_mag
+
+		# apply equal and opposite forces
+			b1.apply_force(force)
+			b2.apply_force(-force)
+			
 	
