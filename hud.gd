2a10d07bfe38cdb795360d36082042f9107479df
Rostyslav Onikiienko <ronikiienko@gmail.com>
2025-11-16 23:33:36 -0800

    w

--- hud.gd
+++ hud.gd
@@ -1,17 +1,16 @@
 extends CanvasLayer
 
-@export var speed_button_group: ButtonGroup
+func _on_speed_1_pressed() -> void:
+	Engine.time_scale = 0.1
 
-func _on_speed_changed(pressed_button):
-	match pressed_button.name:
-		"Speed1":
-			Engine.time_scale = 0.2
-		"Speed2":
-			Engine.time_scale = 0.5
-		"Speed3":
-			Engine.time_scale = 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Hi")
-	speed_button_group.connect("pressed", _on_speed_changed)
+func _on_speed_2_pressed() -> void:
+	Engine.time_scale = 0.2
+
+
+func _on_speed_3_pressed() -> void:
+	Engine.time_scale = 0.5
+
+
+func _on_speed_4_pressed() -> void:
+	Engine.time_scale = 1.0
